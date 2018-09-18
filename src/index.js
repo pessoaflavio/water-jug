dca4905b92b2c0ebcd2a1afbbae4a1fb35e2a162
gianordoli <gianordoligabriel@yahoo.com.br>
2018-09-17 21:16:08 -0400

    Image loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 import './scss/style.scss'
+import cat from './images/cat.jpg';
 
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   return element;
 }
